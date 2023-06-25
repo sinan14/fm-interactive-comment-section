7a47c2adb673bccf5f017d0d5bdf218629981c9f
sinan14 <sinuzar5@gmail.com>
2023-06-25 13:02:59 +0530

    Initial commit

---
+++ e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('fm-interactive-comment-section', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome fm-interactive-comment-section');
+  });
+});
---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="fm-interactive-comment-section" />
+      <div />
+    </>
+  );
+}
+
+export default App;
